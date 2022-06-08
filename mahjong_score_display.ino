c504bd046fbea3e359e18d9078d8dab2fdc5bd06
tj16kimura <kawalab.19.kimura@gmail.com>
2022-06-08 22:26:42 +0900

    tab to spaces

--- mahjong_score_display.ino
+++ mahjong_score_display.ino
@@ -5,22 +5,22 @@
 #define PIN_LATCH 11
 
 struct ScorePin {
-	int digit;
-	int sign;
+  int digit;
+  int sign;
 };
 
 struct WindScorePins {
-	ScorePin east;
-	ScorePin south;
-	ScorePin west;
-	ScorePin north;
+  ScorePin east;
+  ScorePin south;
+  ScorePin west;
+  ScorePin north;
 };
 
 struct Winds {
-	int east;
-	int south;
-	int west;
-	int north;
+  int east;
+  int south;
+  int west;
+  int north;
 };
 
 WindScorePins scorePins = { {5, 4}, {7, 6}, {9, 8}, {15, 14} };
@@ -29,9 +29,9 @@ volatile Winds scores = { 250, 250, 250, 250 };
 AE7SEGGPIO ae7seg(PIN_LATCH, PIN_SDI, PIN_SCK);
 
 void setup() {
-	// 割り込みピン2
-	attachInterrupt(0, setEastSouthScore, FALLING);
-	displayAllScores();
+  // 割り込みピン2
+  attachInterrupt(0, setEastSouthScore, FALLING);
+  displayAllScores();
 }
 
 void loop() {
@@ -44,29 +44,29 @@ void displayScore(int score) {
 }
 
 void displayAllScores() {
-	ae7seg.beginWrite();
-	displayScore(scores.north);
-	displayScore(scores.west);
-	displayScore(scores.south);
-	displayScore(scores.east);
-	ae7seg.endWrite();
+  ae7seg.beginWrite();
+  displayScore(scores.north);
+  displayScore(scores.west);
+  displayScore(scores.south);
+  displayScore(scores.east);
+  ae7seg.endWrite();
 }
 
 int calcScoreDiff(ScorePin scorePinState) {
-	// LOW: 1000  HIGH: 100
+  // LOW: 1000  HIGH: 100
   bool digit = scorePinState.digit;
-	// LOW: plus  HIGH: minus
+  // LOW: plus  HIGH: minus
   bool sign = scorePinState.sign;
 
-	if (digit && sign) return -1;
-	if (digit && !sign) return 1;
-	if (!digit && sign) return -10;
-	if (!digit && !sign) return 10;
+  if (digit && sign) return -1;
+  if (digit && !sign) return 1;
+  if (!digit && sign) return -10;
+  if (!digit && !sign) return 10;
 }
 
 void setEastSouthScore() {
-	ScorePin eastPinState = { digitalRead(scorePins.east.digit), digitalRead(scorePins.east.sign) };
-	// ScorePin southPinState = { digitalRead(scorePins.south.digit), digitalRead(scorePins.south.sign) };
-	scores.east += calcScoreDiff(eastPinState);
+  ScorePin eastPinState = { digitalRead(scorePins.east.digit), digitalRead(scorePins.east.sign) };
+  // ScorePin southPinState = { digitalRead(scorePins.south.digit), digitalRead(scorePins.south.sign) };
+  scores.east += calcScoreDiff(eastPinState);
   displayAllScores();
 }
